fc9ad992ca89a44c8c4575af7aab5e2606cce73b
Harry <hariharasethi89@gmail.com>
2025-11-16 18:53:34 +0530

    add singly linked list

---
+++ impl/linked-list/single_linkedlist.go
@@ -0,0 +1,138 @@
+package main
+
+import "fmt"
+
+type singleNode struct {
+	value int
+	next  *singleNode
+}
+
+type SinglyLinkedList struct {
+	head *singleNode // pointing to the head of the list
+	size int         // current size of the linked list
+}
+
+var ErrPositionOutOfBound = fmt.Errorf("position out of bound")
+
+func newSingleNode(value int) *singleNode {
+	return &singleNode{
+		value: value,
+		next:  nil,
+	}
+}
+
+func NewSinglyLinkedList() *SinglyLinkedList {
+	return &SinglyLinkedList{
+		head: nil,
+		size: 0,
+	}
+}
+
+// Append a new node to the end of the singly linked list
+func (l *SinglyLinkedList) Append(value int) {
+	// head could be null
+	node := newSingleNode(value)
+
+	if l.head == nil {
+		l.head = node
+		l.size++
+		return
+	}
+	// find the last node
+	current := l.head
+
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = node
+	l.size++
+}
+
+// Insert the node with value to the specified position in the singly linked list, if the
+// position is out of bound then it will return an error value otherwise nil
+func (l *SinglyLinkedList) Insert(pos int, value int) error {
+	if pos < 0 || pos > l.size {
+		return ErrPositionOutOfBound
+	}
+
+	node := newSingleNode(value)
+	current := l.head
+
+	if pos == 0 {
+		node.next = l.head
+		l.head = node
+		l.size++
+		return nil
+	}
+
+	// move the head to the previous position where we want to insert the node
+	for i := 0; i < pos-1; i++ {
+		current = current.next
+	}
+
+	node.next = current.next
+	current.next = node
+	l.size++
+	return nil
+}
+
+// Return the value of the node at the specified position in the list,
+// if the position is out of bound it will return an error with value -1
+func (l *SinglyLinkedList) Get(pos int) (int, error) {
+	if pos < 0 || pos > l.size-1 {
+		return -1, ErrPositionOutOfBound
+	}
+
+	cur := l.head
+	for i := 0; i < pos; i++ {
+		cur = cur.next
+	}
+
+	return cur.value, nil
+}
+
+// Delete the node at the specified position, if the position is out of bound then it returns the error
+func (l *SinglyLinkedList) Delete(pos int) error {
+	if pos < 0 || pos > l.size-1 {
+		return ErrPositionOutOfBound
+	}
+	// if the position is head node
+	if pos == 0 {
+		temp := l.head
+		l.head = temp.next
+
+		// cut the link
+		temp.next = nil
+		l.size--
+		return nil
+	}
+
+	cur := l.head
+	for i := 0; i < pos-1; i++ {
+		cur = cur.next
+	}
+
+	cur.next = cur.next.next
+	l.size--
+	return nil
+}
+
+// Return the size of the linked list
+func (l *SinglyLinkedList) Size() int {
+	return l.size
+}
+
+// Print the content
+func (l *SinglyLinkedList) String() string {
+	cur := l.head
+	out := ""
+
+	for cur != nil {
+		out += fmt.Sprintf("%d -> ", cur.value)
+		cur = cur.next
+	}
+
+	out += "nil"
+	return out
+}
---
+++ impl/linked-list/single_linkedlist_test.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSinglyLinkedList_Append(t *testing.T) {
+	tests := []struct {
+		name     string
+		values   []int
+		expected []int
+	}{
+		{"append single", []int{10}, []int{10}},
+		{"append multiple", []int{1, 2, 3}, []int{1, 2, 3}},
+		{"append negative values", []int{-5, -10}, []int{-5, -10}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ll := NewSinglyLinkedList()
+			for _, v := range tt.values {
+				ll.Append(v)
+			}
+
+			if ll.Size() != len(tt.expected) {
+				t.Fatalf("expected size %d, got %d", len(tt.expected), ll.Size())
+			}
+
+			// verify order
+			for i, exp := range tt.expected {
+				val, _ := ll.Get(i)
+				if val != exp {
+					t.Fatalf("at index %d: expected %d, got %d", i, exp, val)
+				}
+			}
+		})
+	}
+}
+
+func TestSinglyLinkedList_Insert(t *testing.T) {
+	tests := []struct {
+		name       string
+		initial    []int
+		pos        int
+		value      int
+		expectErr  bool
+		finalState []int
+	}{
+		{
+			"insert at head",
+			[]int{2, 3},
+			0,
+			1,
+			false,
+			[]int{1, 2, 3},
+		},
+		{
+			"insert at middle",
+			[]int{1, 3},
+			1,
+			2,
+			false,
+			[]int{1, 2, 3},
+		},
+		{
+			"insert at end",
+			[]int{1, 2},
+			2,
+			3,
+			false,
+			[]int{1, 2, 3},
+		},
+		{
+			"insert out of bounds",
+			[]int{1, 2},
+			5,
+			10,
+			true,
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ll := NewSinglyLinkedList()
+			for _, v := range tt.initial {
+				ll.Append(v)
+			}
+
+			err := ll.Insert(tt.pos, tt.value)
+			if tt.expectErr && err == nil {
+				t.Fatalf("expected error but got nil")
+			}
+
+			if !tt.expectErr {
+				if ll.Size() != len(tt.finalState) {
+					t.Fatalf("expected size %d, got %d", len(tt.finalState), ll.Size())
+				}
+
+				for i, exp := range tt.finalState {
+					got, _ := ll.Get(i)
+					if got != exp {
+						t.Fatalf("expected %d at pos %d, got %d", exp, i, got)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestSinglyLinkedList_Get(t *testing.T) {
+	ll := NewSinglyLinkedList()
+	ll.Append(1)
+	ll.Append(2)
+	ll.Append(3)
+
+	tests := []struct {
+		pos       int
+		expect    int
+		expectErr bool
+	}{
+		{0, 1, false},
+		{1, 2, false},
+		{2, 3, false},
+		{3, -1, true},
+		{-1, -1, true},
+	}
+
+	for _, tt := range tests {
+		got, err := ll.Get(tt.pos)
+		if tt.expectErr {
+			if err == nil {
+				t.Errorf("expected error for pos %d but got nil", tt.pos)
+			}
+			continue
+		}
+
+		if got != tt.expect {
+			t.Errorf("expected %d, got %d", tt.expect, got)
+		}
+	}
+}
+
+func TestSinglyLinkedList_Delete(t *testing.T) {
+	tests := []struct {
+		name       string
+		initial    []int
+		pos        int
+		expectErr  bool
+		finalState []int
+	}{
+		{
+			"delete head",
+			[]int{1, 2, 3},
+			0,
+			false,
+			[]int{2, 3},
+		},
+		{
+			"delete middle",
+			[]int{1, 2, 3},
+			1,
+			false,
+			[]int{1, 3},
+		},
+		{
+			"delete last",
+			[]int{1, 2, 3},
+			2,
+			false,
+			[]int{1, 2},
+		},
+		{
+			"delete out of bounds",
+			[]int{1, 2},
+			5,
+			true,
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ll := NewSinglyLinkedList()
+
+			for _, v := range tt.initial {
+				ll.Append(v)
+			}
+
+			err := ll.Delete(tt.pos)
+			if tt.expectErr {
+				if err == nil {
+					t.Fatalf("expected error but got nil")
+				}
+				return
+			}
+
+			if ll.Size() != len(tt.finalState) {
+				t.Fatalf("expected size %d, got %d", len(tt.finalState), ll.Size())
+			}
+
+			for i, exp := range tt.finalState {
+				got, _ := ll.Get(i)
+				if got != exp {
+					t.Fatalf("expected %d at %d, got %d", exp, i, got)
+				}
+			}
+		})
+	}
+}
